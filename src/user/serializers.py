3092238a0a8a8dd458c7cd7cab46f1c1b9645615
SemihDurmus <semihdurmus@yahoo.com>
2021-01-20 23:59:58 +0100

    Add: User delete + password update

--- src/user/serializers.py
+++ src/user/serializers.py
@@ -55,3 +55,43 @@ class ProfileSerializer(serializers.ModelSerializer):
             "image",
             "bio",
         )
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True, required=True, validators=[
+                                     validate_password], style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, required=True, style={
+                                      'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'password2',
+        ]
+
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "password2": {"write_only": True},
+            "username": {"read_only": True},
+            "email": {"read_only": True},
+        }
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password didnt match"}
+            )
+        return attrs
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
--- src/user/urls.py
+++ src/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView, ProfileView
+from .views import RegisterView, ProfileView, UserGetUpdateDelete
 
 urlpatterns = [
     path('register/', RegisterView),
     path('<int:id>/profile/', ProfileView),
+    path('<int:id>/edit/', UserGetUpdateDelete),
 ]
